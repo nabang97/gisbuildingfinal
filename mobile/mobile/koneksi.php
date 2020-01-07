b390de3818f1e1d05308308f4e8888cd2006de5f
nabang97 <parawitayolanda@gmail.com>
2020-01-08 03:29:34 +0700

    gfg

--- mobile/mobile/koneksi.php
+++ mobile/mobile/koneksi.php
@@ -6,11 +6,11 @@
 	$port = "5432";
 	$dbname = "afwubqsz";
 
-	$host = "localhost";
-	$user = "postgres";
-	$pass = "root";
-	$port = "5432";
-	$dbname = "gibuilding";
+	// $host = "localhost";
+	// $user = "postgres";
+	// $pass = "root";
+	// $port = "5432";
+	// $dbname = "gibuilding";
 
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 
