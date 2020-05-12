329e5feb490729ef4f8b059f357bfb0b84fdfe6d
Ridho Darman <33389796+ridhodarman@users.noreply.github.com>
2020-05-12 10:11:25 +0700

    Update login.php

--- act/login.php
+++ act/login.php
@@ -4,7 +4,6 @@
 		$user = pg_escape_string($_POST['username']);
 		$pass = pg_escape_string(md5($_POST['password']));
 		$a=array($user,$pass);
-		var_dump($a);
 		//$sql = pg_query_params($conn,'SELECT * FROM user_account where username="?" and password="?"', array('$user','$pass')) or die('Could not connect: ' . pg_last_error());
 		$sql = pg_query("SELECT * FROM user_account where username='$user' and password='$pass'") or die('Could not connect: ' . pg_last_error());
 		$row= pg_fetch_array($sql);
